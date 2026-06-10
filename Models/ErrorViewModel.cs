aac11c1f8da8f9fe9d51c30213d5744773f577a4
nicoly-athayde <nicolyathayde4@gmail.com>
2026-06-10 11:14:41 -0300

    css está errado

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace By_Samir_Vitrine.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
