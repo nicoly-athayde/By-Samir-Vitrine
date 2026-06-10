1bf93d1518a8da4790a1443695fc3c93298d5e0e
nicoly-athayde <nicolyathayde4@gmail.com>
2026-06-10 11:43:43 -0300

    Arrumar o cabeçalho da home

--- obj/Debug/net10.0/By-Samir-Vitrine.AssemblyInfo.cs
+++ obj/Debug/net10.0/By-Samir-Vitrine.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("By-Samir-Vitrine")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aac11c1f8da8f9fe9d51c30213d5744773f577a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("By-Samir-Vitrine")]
 [assembly: System.Reflection.AssemblyTitleAttribute("By-Samir-Vitrine")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
